caf10c1aaaf77a7fc17ffcf2785eb54ac03fd373
jokavanni <n3vajo00@students.oamk.fi>
2024-02-02 13:16:34 +0200

    Javascriptiä muokattu niin että noppa pyörii yhä uudelleen.

--- JS/functions.js
+++ JS/functions.js
@@ -1,10 +1,7 @@
-const dice = document.querySelector('#dice')
+const div = document.querySelector('#dice')
+const img = document.querySelector('img')
 dice.addEventListener('click', () => {
-let rollResult = Math.floor((Math.random() * 6) + 1);
-if (rollResult == 1) document.write('<img src="./img/1.png">');
-else if (rollResult == 2) document.write('<img src="./img/2.png">');
-else if (rollResult == 3) document.write('<img src="./img/3.png">');
-else if (rollResult == 4) document.write('<img src="./img/4.png">');
-else if (rollResult == 5) document.write('<img src="./img/5.png">');
-else  document.write('<img src="./img/6.png">');
+let rollResult = Math.floor((Math.random() * 6) + 1)
+img.src= `./img/${rollResult}.png`
 })
+
